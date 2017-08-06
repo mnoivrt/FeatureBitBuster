bf25b7f9ec9c2a5dd8a3718bb4bd43b347084117
aran <aran@innovid.com>
2017-08-06 11:49:45 +0300

    Added interface for handlers + first implementation of bool statement handler

---
+++ src/main/java/BooleanStatementHandler.java
@@ -0,0 +1,11 @@
+import com.github.javaparser.ast.stmt.Statement;
+
+/**
+ * Created by aran on 06/08/2017.
+ */
+public class BooleanStatementHandler implements IStatementHandler {
+    public Statement Execute(Statement statement)
+    {
+       return statement;
+    }
+}
---
+++ src/main/java/IStatementHandler.java
@@ -0,0 +1,8 @@
+import com.github.javaparser.ast.stmt.Statement;
+
+/**
+ * Created by aran on 06/08/2017.
+ */
+public interface IStatementHandler {
+    Statement Execute(Statement statement);
+}
