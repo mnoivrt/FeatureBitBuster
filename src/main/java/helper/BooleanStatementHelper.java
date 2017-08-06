a2960b8f14ba9d8dd79e8c86716edad9f9aa3f6b
aran <aran@innovid.com>
2017-08-06 15:57:19 +0300

    Implement the first ever binary helper

--- src/main/java/helper/BooleanStatementHelper.java
+++ src/main/java/helper/BooleanStatementHelper.java
@@ -1,11 +1,15 @@
 package helper;
 
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.ast.expr.Expression;
 
 public class BooleanStatementHelper {
 
-    public BinaryExpr eval(BinaryExpr binaryExpr) {
-        return binaryExpr;
+    public Expression eval(BinaryExpr binaryExpr, String fbName) {
+        Node parent = binaryExpr.getParentNode().get();
+        return ((BinaryExpr) binaryExpr).getRight();
+
     }
 }
 
