0845adbab5c39d69900b621b148bdaae83c6de7d
Aviv <aviv.alfasa@innovid.com>
2017-08-06 14:13:23 +0300

    added java file traverse to FileBuster

--- src/main/java/FileBuster.java
+++ src/main/java/FileBuster.java
@@ -7,8 +7,12 @@ import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.Statement;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Created by moshe on 06/08/2017.
@@ -21,6 +25,49 @@ public class FileBuster {
     public FileBuster(String path, String fbName){
         this.fbName = fbName;
         this.path = path;
+    }
+
+    public void bust(){
+        File[] files = new File(path).listFiles();
+        traverseFiles(files);
+    }
+
+    private void traverseFiles(File[] files) {
+        for (File file : files) {
+            String fileName = file.getName();
+
+            if (file.isDirectory()) {
+                traverseFiles(file.listFiles());
+            } else {
+                String fileExtension = getFileExtension(fileName);
+                if(fileExtension.equals("java"))
+                    if(isFileContainsFb(file))
+                        System.out.println("returned File: " + file.getAbsolutePath());
+            }
+        }
+    }
+
+    private boolean isFileContainsFb(File file) {
+        boolean fileContainsFb = false;
+        try{
+            Path filePath = Paths.get(file.getName());
+            String content = new String(Files.readAllBytes(filePath));
+            fileContainsFb = content.contains(fbName);
+        }catch (Exception ex){
+            System.out.println(ex.toString());
+        }
+        return fileContainsFb;
+    }
+
+    private static String getFileExtension(String fileName){
+        String extension = "";
+
+        int i = fileName.lastIndexOf('.');
+        if (i > 0) {
+            extension = fileName.substring(i+1);
+        }
+
+        return extension;
     }
 
     public void RemoveFeatureBitFromFile() throws FileNotFoundException {
