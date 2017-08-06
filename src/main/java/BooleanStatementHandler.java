7fbf58b1383588f36d2baf1ca1938ec72331c3dc
goor <goor@innovid.com>
2017-08-06 12:05:51 +0300

    Added fb name to IStatementHandler interface

--- src/main/java/BooleanStatementHandler.java
+++ src/main/java/BooleanStatementHandler.java
@@ -4,7 +4,7 @@ import com.github.javaparser.ast.stmt.Statement;
  * Created by aran on 06/08/2017.
  */
 public class BooleanStatementHandler implements IStatementHandler {
-    public Statement Execute(Statement statement)
+    public Statement Execute(Statement statement, String fbName)
     {
        return statement;
     }
--- src/main/java/IStatementHandler.java
+++ src/main/java/IStatementHandler.java
@@ -4,5 +4,5 @@ import com.github.javaparser.ast.stmt.Statement;
  * Created by aran on 06/08/2017.
  */
 public interface IStatementHandler {
-    Statement Execute(Statement statement);
+    Statement Execute(Statement statement, String fbName);
 }
