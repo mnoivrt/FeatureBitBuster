f409fff735b3bd35be184c528aa04660e9f259a4
aran <aran@innovid.com>
2017-08-06 16:14:07 +0300

    Implement the first ever binary handler - now FOR REAL

--- src/main/java/BooleanStatementHandler.java
+++ src/main/java/BooleanStatementHandler.java
@@ -1,8 +1,12 @@
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.ExpressionStmt;
 import com.github.javaparser.ast.stmt.Statement;
+import helper.BooleanStatementHelper;
 
 /**
  * Created by aran on 06/08/2017.
@@ -12,16 +16,27 @@ public class BooleanStatementHandler implements IStatementHandler {
     {
         if(IsBooleanStatement(statement) && IsContainsFb(fbName, statement))
         {
+            BooleanStatementHelper helper = new BooleanStatementHelper();
+            Expression newCondition = helper.eval((BinaryExpr)ExtractBoolCondition(statement), fbName);
+            UpdateBoolCondition(statement, newCondition);
         }
+    }
 
+    private void UpdateBoolCondition(Statement statement, Expression newCondition)
+    {
+        ((VariableDeclarator)((VariableDeclarationExpr)((ExpressionStmt) statement).getExpression()).getVariable(0)).setInitializer(newCondition);
+    }
 
+    private Expression ExtractBoolCondition(Statement statement)
+    {
+        return ((VariableDeclarator)((VariableDeclarationExpr)((ExpressionStmt) statement).getExpression()).getVariable(0)).getInitializer().get();
     }
 
     private boolean IsBooleanStatement(Statement statement)
     {
         return
                 statement instanceof ExpressionStmt &&
-                ((ExpressionStmt) statement).getExpression() instanceof VariableDeclarationExpr;
+                        ((ExpressionStmt) statement).getExpression() instanceof VariableDeclarationExpr;
 
     }
 
--- src/main/java/helper/BooleanStatementHelper.java
+++ src/main/java/helper/BooleanStatementHelper.java
@@ -12,13 +12,3 @@ public class BooleanStatementHelper {
 
     }
 }
-
-
-// 1.       foreach leaf
-// 1.1          if leaf is fb
-// 1.1.1            if cond is &&
-// 1.1.1.1              change parent node to other child
-// 1.1.2            if cond is ||
-// 1.1.2.1              change parent to true
-// 1.1.3            if cond is !
-// 1.1.3.1
