e86e0d1afd706aedf09e41c15e1529fbe0c878d7
aran <aran@innovid.com>
2017-08-06 12:14:01 +0300

    Update statement handlers to return Node instead of Statement

--- src/main/java/BooleanStatementHandler.java
+++ src/main/java/BooleanStatementHandler.java
@@ -1,11 +1,12 @@
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.stmt.Statement;
 
 /**
  * Created by aran on 06/08/2017.
  */
 public class BooleanStatementHandler implements IStatementHandler {
-    public Statement Execute(Statement statement, String fbName)
+    public Node Execute(Statement statement, String fbName)
     {
-       return statement;
+       return statement.getChildNodes().get(0);
     }
 }
--- src/main/java/IStatementHandler.java
+++ src/main/java/IStatementHandler.java
@@ -1,8 +1,9 @@
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.stmt.Statement;
 
 /**
  * Created by aran on 06/08/2017.
  */
 public interface IStatementHandler {
-    Statement Execute(Statement statement, String fbName);
+    Node Execute(Statement statement, String fbName);
 }
