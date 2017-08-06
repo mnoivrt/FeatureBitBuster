35863a194b82f7208f091868a4a465fc80e5f2f3
aran <aran@innovid.com>
2017-08-06 13:06:57 +0300

    Implemented the bool statement handler with super cool instanceof manipulcation

--- src/main/java/BooleanStatementHandler.java
+++ src/main/java/BooleanStatementHandler.java
@@ -1,4 +1,6 @@
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+import com.github.javaparser.ast.stmt.ExpressionStmt;
 import com.github.javaparser.ast.stmt.Statement;
 
 /**
@@ -7,6 +9,23 @@ import com.github.javaparser.ast.stmt.Statement;
 public class BooleanStatementHandler implements IStatementHandler {
     public Node Execute(Statement statement, String fbName)
     {
-       return statement.getChildNodes().get(0);
+        if(IsBooleanStatement(statement) && IsContainsFb(fbName, statement))
+        {
+            return statement.getChildNodes().get(0);
+        }
+        return null;
+
+    }
+
+    private boolean IsBooleanStatement(Statement statement)
+    {
+        return
+                statement instanceof ExpressionStmt &&
+                ((ExpressionStmt) statement).getExpression() instanceof VariableDeclarationExpr;
+
+    }
+
+    private static boolean IsContainsFb(String fbName, Statement statement){
+        return statement.getTokenRange().toString().contains(fbName);
     }
 }
