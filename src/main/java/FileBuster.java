d5657e2d8c05139b8f4446233f17b39a83a04eaf
Aviv <aviv.alfasa@innovid.com>
2017-08-06 15:05:00 +0300

    moved traverse logic to FileScanner

--- src/main/java/FileBuster.java
+++ src/main/java/FileBuster.java
@@ -7,12 +7,8 @@ import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.Statement;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
  * Created by moshe on 06/08/2017.
@@ -22,52 +18,9 @@ public class FileBuster {
     String fbName;
     String path;
 
-    public FileBuster(String path, String fbName){
+    public FileBuster(String path, String fbName) {
         this.fbName = fbName;
         this.path = path;
-    }
-
-    public void bust(){
-        File[] files = new File(path).listFiles();
-        traverseFiles(files);
-    }
-
-    private void traverseFiles(File[] files) {
-        for (File file : files) {
-            String fileName = file.getName();
-
-            if (file.isDirectory()) {
-                traverseFiles(file.listFiles());
-            } else {
-                String fileExtension = getFileExtension(fileName);
-                if(fileExtension.equals("java"))
-                    if(isFileContainsFb(file))
-                        System.out.println("returned File: " + file.getAbsolutePath());
-            }
-        }
-    }
-
-    private boolean isFileContainsFb(File file) {
-        boolean fileContainsFb = false;
-        try{
-            Path filePath = Paths.get(file.getName());
-            String content = new String(Files.readAllBytes(filePath));
-            fileContainsFb = content.contains(fbName);
-        }catch (Exception ex){
-            System.out.println(ex.toString());
-        }
-        return fileContainsFb;
-    }
-
-    private static String getFileExtension(String fileName){
-        String extension = "";
-
-        int i = fileName.lastIndexOf('.');
-        if (i > 0) {
-            extension = fileName.substring(i+1);
-        }
-
-        return extension;
     }
 
     public void RemoveFeatureBitFromFile() throws FileNotFoundException {
